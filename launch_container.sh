831ab3da076d9bff11427382be6d977648462e1f
CarlosEDP <me@carlosedp.com>
2018-08-19 17:00:30 +0100

    Adjust for Automaton modpack

--- launch_container.sh
+++ launch_container.sh
@@ -4,9 +4,11 @@ mkdir ~/mc-data && chmod 777 ~/mc-data
 docker run -d -it \
     -v ~/mc-data:/data \
     -p 25565:25565 \
+    -p 25575:25575 \
     -e VERSION=1.12.2 \
     -e TYPE=FORGE \
-    -e FORGEVERSION=14.23.4.2739 \
+    -e FORGEVERSION=14.23.4.2705 \
+    -e MEMORY=3G \
     --name mc \
     carlosedp/minecraft-server:arm64
 
